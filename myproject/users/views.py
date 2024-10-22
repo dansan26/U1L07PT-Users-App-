ed9d4e9af7c6ac638cf8b0320cf6aa3d9f4ac54c
Daniel Sanjuan <danielbacan2008@gmail.com>
2024-10-22 09:56:21 -0400

    Completed

--- myproject/users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def registration(request):
-    context = {
-        'active_link': 'users',
-    }
-    return render(request, 'register/register.html', context)
