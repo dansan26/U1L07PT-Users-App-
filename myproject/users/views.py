bfcc5f4897350c513432470e60d83e3f6a01595b
Daniel Sanjuan <danielbacan2008@gmail.com>
2024-10-22 10:30:44 -0400

    10-22 Classwork

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def registration(request):
+    # Check if the form is submitted
+    if request.method == "POST":
+        form = UserCreationForm(request.POST) # Submitted with data
+        # Validate Form
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else: #Get method (Empty form)
+        form = UserCreationForm()
     context = {
         'active_link': 'users',
+        'form': form,
     }
     return render(request, 'register/register.html', context)
